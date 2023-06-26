f2aa73d93c4b4cbef27d0627f18e1bc8631779f0
lol8344984 <136927361+lol8344984@users.noreply.github.com>
2023-06-26 11:25:03 -0400

    Update send.lua

--- send.lua
+++ send.lua
@@ -1,27 +1,26 @@
-wait(4)
-
-local ScreenGui = Instance.new("ScreenGui")
-local Frame = Instance.new("Frame")
-local _1 = Instance.new("TextLabel")
-ScreenGui.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
-ScreenGui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
-Frame.Parent = ScreenGui
-Frame.BackgroundTransparency = 0.250
-Frame.Position = UDim2.new(0.863671064, 0, 0.830614805, 0)
-Frame.Size = UDim2.new(0, 261, 0, 74)
-_1.Name = "1"
-_1.Parent = Frame
-_1.BackgroundTransparency = 1.000
-_1.Position = UDim2.new(0.00766283507, 0, -0.0135135138, 0)
-_1.Size = UDim2.new(0, 259, 0, 75)
-_1.Font = Enum.Font.SourceSans
-_1.Text = "Powered by Fast1nject"
-_1.TextColor3 = Color3.fromRGB(250, 250, 250)
-_1.TextSize = 23.000
-
+-- FAST1NJECT
+local gui = Instance.new("ScreenGui")
+gui.Name = "RainbowNotification"
+gui.Parent = game.Players.LocalPlayer.PlayerGui
+local frame = Instance.new("Frame")
+frame.Size = UDim2.new(0, 200, 0, 50)
+frame.Position = UDim2.new(1, -220, 0, 20)
+frame.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+frame.BorderSizePixel = 0
+frame.Parent = gui
+local textLabel = Instance.new("TextLabel")
+textLabel.Size = UDim2.new(1, 0, 1, 0)
+textLabel.BackgroundTransparency = 1
+textLabel.Text = "Powered by Fast1Inject by coolpro1"
+textLabel.Font = Enum.Font.SourceSansBold
+textLabel.TextColor3 = Color3.fromRGB(0, 0, 0)
+textLabel.TextScaled = true
+textLabel.Parent = frame
 local hue = 0
-while Frame.Parent do
-    hue = (hue + 1) % 360
-    Frame.BackgroundColor3 = Color3.fromHSV(hue / 360, 1, 1)
-    wait(0.05)
+local function updateColor()
+    hue = (hue + 2) % 360
+    frame.BackgroundColor3 = Color3.fromHSV(hue / 360, 1, 1)
 end
+game:GetService("RunService").Heartbeat:Connect(updateColor)
+wait(4.69)
+gui:Destroy()
